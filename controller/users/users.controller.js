37ba6cb4e11390ab83e62a164994d27621e57ee0
Authachai Murnpimai <premelar7@gmail.com>
2021-10-21 00:43:57 +0700

    Create Controller/Route/Config/Model

---
+++ controller/users/users.controller.js
@@ -0,0 +1,8 @@
+module.exports = {
+    users_create: function (req, res) {
+        res.status(200).json(req.body);
+    },
+    users_update: function (req, res) {
+        res.status(200).json(req.body);
+    }
+};
--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require("express");
-const logger = require("./middleware/LogMiddleware");
+const logger = require("./middleware/log.middleware");
 const app = express();
+
+const api = require("./routes/main");
 
 const port = process.env.PORT || 3000;
 
 //MIDDILEWARE
 app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-//ROUTE PATH
+//DEFAULT PATH
 app.get("/", (req, res) => {
     res.send("Express.js Framework development.");
 });
+
+//API PATH
+app.use("/api", api);
 
 //RUN NODE SERVER
 app.listen(port, () => {
