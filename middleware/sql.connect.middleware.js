62c07f1281bee0b5e6254cc9574ca5e83c22719c
Authachai Murnpimai <premelar7@gmail.com>
2021-10-22 00:29:43 +0700

    Update Model Database

--- middleware/sql.connect.middleware.js
+++
@@ -1,7 +0,0 @@
-const sql = require("./../config/databse.config");
-
-module.exports = async (req, res, next) => {
-    let connection = await sql.ConnnectMySQL();
-    req["mysql"] = connection;
-    next();
-}
