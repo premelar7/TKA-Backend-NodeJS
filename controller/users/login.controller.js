14f9a07f1edde047c1814da93d4cc2fdcd9673e8
nnicha <nichakorn9906@gmail.com>
2021-12-04 21:25:58 +0700

    Token + Rate limit

--- controller/users/login.controller.js
+++ controller/users/login.controller.js
@@ -3,15 +3,14 @@ const moment = require("moment");
 const bcrypt = require("bcrypt");
 const user_portal = db.user_portal;
 
-const response = {
-    success: false,
-    code: null,
-    http_status: 200,
-    result: []
-};
-
 module.exports = {
     login: async function (req, res) {
+        let response = {
+            success: false,
+            code: null,
+            http_status: 200,
+            result: []
+        };
         let data_login = await user_portal.findAll({
             //raw:true,
             where: {
@@ -21,6 +20,8 @@ module.exports = {
         if (data_login.length > 0) {
             let match = await bcrypt.compare(req.body.ps, data_login[0].password);
             if (match) {
+                let jwt = require('jsonwebtoken');
+                let token = jwt.sign({ user_id: data_login[0].user_id }, process.env.TOKEN_KEY, { expiresIn: '1h' });
                 await user_portal.update({
                     lastlogin: moment().format('YYYY-MM-DD h:mm:ss')
                 },
@@ -29,20 +30,22 @@ module.exports = {
                             email: req.body.email
                         }
                     });
-                response.result = ["Login Success!"];
+                response.result = [{ access_token: token }];
                 response.success = true;
-                response.code = 4000; //4000 = Login Success!
+                response.code = 40101;
             }
             else {
                 response.result = ["Email or password incorrect."];
                 response.success = false;
-                response.code = 3000;
+                response.code = 30101;
+                response.http_status = 422;
             }
         }
         else {
             response.result = ["Email or password incorrect."];
             response.success = false;
-            response.code = 3000; //3000 = Email or password incorrect
+            response.code = 30101;
+            response.http_status = 422;
         }
         res.status(response.http_status).json(response);
     }
--- routes/main.js
+++ routes/main.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require("express-rate-limit");
 const app = express();
 
 const users_api = require("./api/users");
@@ -6,8 +7,19 @@ const authen_api = require("./api/authen");
 const login_api = require("./api/login");
 
 const logger = require("./../middleware/log.middleware");
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: {
+        success: false,
+        code: null,
+        http_status: 429,
+        result: ["Too many accounts created from this IP, please try again after an hour"]
+    }
+});
 
 app.use(logger);
+app.use(apiLimiter);
 app.use("/v1/users", users_api);
 app.use("/v1/authen", authen_api);
 app.use("/v1/login", login_api);
